015546a9f2b22a757cbab13501bf214379b8f3eb
Francesco Rollo <eferollo@gmail.com>
2024-03-10 19:32:25 +0100

    01_rand

---
+++ OPENSSL/01_rand/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
+
+#define MAX 128
+
+void handle_errors(){
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+int main(){
+
+    unsigned char random_string[MAX];
+
+    if(RAND_load_file("/dev/random", 64) != 64)
+        handle_errors();
+
+    if(RAND_bytes(random_string, MAX) != 1)
+        handle_errors();
+
+    printf("Sequence generated: \n");
+    for(int i = 0; i < MAX; i++)
+        printf("%02x-", random_string[i]);
+
+    return 0;
+}
