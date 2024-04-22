b485c7ae4ab74eaa5650cf2811dca2abc0f2e7b4
Francesco Rollo <eferollo@gmail.com>
2024-04-22 15:12:14 +0200

    forge token solved

---
+++ CTF/crypto-symmetric/forge-cookie/solve.py
@@ -0,0 +1,51 @@
+import json
+import os
+os.environ['PWNLIB_NOTERM'] = 'True'
+os.environ['PWNLIB_SILENT'] = 'True'
+import pwn
+import base64
+
+server = pwn.remote("130.192.5.212", 6521)
+
+mod = json.dumps({"admin": True})
+mod = mod.encode()
+print(mod)
+
+# Hi, please tell me your name! -> provide name
+print(server.recvline().decode())
+server.sendline("userA".encode())
+
+# Get the plaintext token
+plain_token = server.recvline().decode().strip().split("> ")[1]
+plain_token = bytearray(plain_token.encode())
+print(plain_token)
+
+# nonce + token
+rec_data = server.recvline().decode().strip().split(": ")[1]
+nonce, enc_token = rec_data.split(".")
+print("Nonce= " + nonce)
+print("Token= " + enc_token)
+# decode from base64
+enc_token = base64.b64decode(enc_token)
+print(f"Token decoded from base64= {enc_token}")
+
+# skip the menu
+for output in server.recvlines(4):
+    print(output.decode())
+
+# send option flag
+server.sendline("flag".encode())
+# skip "What is your token"
+print(server.recvline().decode())
+
+token = bytearray()
+for t_b, p_b, m_b in zip(enc_token, plain_token, mod):
+    token.append(t_b ^ m_b ^ p_b)
+
+token = base64.b64encode(token).decode()
+tosend = f"{nonce}.{token}"
+print(tosend)
+# send token to the server
+server.sendline(tosend.encode())
+for output in server.recvlines(3):
+    print(output.decode())
